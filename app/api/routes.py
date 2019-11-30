0090f64af15c7df06c81050be183874672034d51
Stepan <sd.denisoff@gmail.com>
2019-12-01 01:09:11 +0300

    basic web

--- app/api/routes.py
+++ app/api/routes.py
@@ -5,3 +5,6 @@ api = Blueprint('api', __name__, template_folder='../templates', static_folder='
 
 from app.api.controllers.index import IndexPageController
 api.add_url_rule('/', view_func=IndexPageController.as_view('IndexPage'))
+
+from app.api.controllers.map import MapController
+api.add_url_rule('/map', view_func=MapController.as_view('Map'))
