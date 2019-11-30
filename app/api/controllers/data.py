f693dfea91de207e81dd6d41edbe0dc0920ffff3
Stepan <sd.denisoff@gmail.com>
2019-12-01 01:50:48 +0300

    fixed data

--- app/api/controllers/data.py
+++ app/api/controllers/data.py
@@ -1,8 +1,15 @@
 from flask.views import MethodView
+import json
 
 
 class DataController(MethodView):
     def get(self):
         with open('DataAnalysis/data/coords.json', 'r', encoding='utf-8') as input:
-            coords = input.read()
+            data = json.load(input)
+        coords = list()
+        i = 0
+        for c in data.values():
+            if i not in [407, 370, 214, 356, 357, 259, 390, 244, 293, 388, 324, 412, 261]:
+                coords.append(c)
+        coords = json.dumps(coords)
         return coords
--- app/api/controllers/map.py
+++ app/api/controllers/map.py
@@ -5,6 +5,4 @@ from config import apikey
 
 class MapController(MethodView):
     def get(self):
-        with open('DataAnalysis/data/coords.json', 'r', encoding='utf-8') as input:
-            coords = input
-        return render_template('map.html', apikey=apikey, coords=coords)
+        return render_template('map.html', apikey=apikey)
