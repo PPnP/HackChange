2f0fd3b0de2e463eaba069973310ab30cceae689
Stepan <sd.denisoff@gmail.com>
2019-12-01 01:42:55 +0300

    fixed data

--- app/api/controllers/data.py
+++ app/api/controllers/data.py
@@ -4,5 +4,5 @@ from flask.views import MethodView
 class DataController(MethodView):
     def get(self):
         with open('DataAnalysis/data/coords.json', 'r', encoding='utf-8') as input:
-            coords = input
+            coords = input.read()
         return coords
