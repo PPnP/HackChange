ee92666150a8bd04ce99eb2764c0d1d3436c4e88
Stepan <sd.denisoff@gmail.com>
2019-12-01 01:16:50 +0300

    coords to front

--- app/api/controllers/map.py
+++ app/api/controllers/map.py
@@ -1,7 +1,15 @@
 from flask.views import MethodView
 from flask import render_template
+import json
 
 
 class MapController(MethodView):
     def get(self):
-        return render_template('map.html')
+        with open('DataAnalysis/data/coords.json', 'r', encoding='utf-8') as input:
+            data = json.load(input)
+        coords = list()
+        i = 0
+        for c in data.values():
+            if i not in [407, 370, 214, 356, 357, 259, 390, 244, 293, 388, 324, 412, 261]:
+                coords.append(c)
+        return render_template('map.html', coords=coords)
