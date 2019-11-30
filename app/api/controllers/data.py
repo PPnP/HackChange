338e7c60ac8e49d0dacc98cb0169a097067c064c
Stepan <sd.denisoff@gmail.com>
2019-12-01 01:36:52 +0300

    data

--- app/api/controllers/data.py
+++ app/api/controllers/data.py
@@ -1,10 +1,8 @@
 from flask.views import MethodView
-from flask import render_template
-from config import apikey
 
 
-class MapController(MethodView):
+class DataController(MethodView):
     def get(self):
         with open('DataAnalysis/data/coords.json', 'r', encoding='utf-8') as input:
             coords = input
-        return render_template('map.html', apikey=apikey, coords=coords)
+        return coords
--- app/api/routes.py
+++ app/api/routes.py
@@ -8,3 +8,6 @@ api.add_url_rule('/', view_func=IndexPageController.as_view('IndexPage'))
 
 from app.api.controllers.map import MapController
 api.add_url_rule('/map', view_func=MapController.as_view('Map'))
+
+from app.api.controllers.data import DataController
+api.add_url_rule('/data', view_func=DataController.as_view('Data'))
