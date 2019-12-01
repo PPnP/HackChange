4b38f34a226fa0e2bfb52a43b4d8b93e66e38206
Stepan <sd.denisoff@gmail.com>
2019-12-01 04:04:10 +0300

    adding param

--- DataAnalysis/geocoder.py
+++ DataAnalysis/geocoder.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import requests
 import json
+from math import isnan
 
 
 class Geocoder(object):
@@ -13,12 +14,18 @@ class Geocoder(object):
 
     def get_coords(self, data, f_out):
         coords = dict()
-        for addr in data:
-            Geocoder.params['geocode'] = addr
+        for index, row in data.iterrows(): # for addr in data: | use it for coding post offices data
+            Geocoder.params['geocode'] = row['ADDRESS']
             r = requests.get(Geocoder.main_url, params=Geocoder.params)
             c = self.processing(r)
             if c is not None and 53.1 < float(c[0]) < 55.5 and 33 < float(c[1]) < 40:
-                coords[addr] = c
+                postamat = 0                            # use 6 lines below only for coding initial data
+                if not isnan(row['Postamat_daily']):
+                    postamat = 1
+                cashbox = 0
+                if not isnan(row['cashbox_daily']):
+                    cashbox = 1
+                coords[row['ADDRESS']] = c + [str(postamat) + str(cashbox)]  # coords[row['ADDRESS']] = c | use it for coding post offices data
         with open(f_out, 'w') as output:
             json.dump(coords, output)
 
@@ -41,12 +48,11 @@ class Geocoder(object):
 gc = Geocoder()
 
 # initial_data geocoder
-# pd = pd.read_excel('data/initial_data.xlsx')
-# addresses = list(pd['ADDRESS'])
-# gc.get_coords(addresses, 'data/coords.json')
+pd = pd.read_excel('data/initial_data.xlsx')
+gc.get_coords(pd, 'data/coords.json')
 
 # post offices geocoder
-f = open('data/post_offices.txt', 'r')
-addresses = list([addr.strip() for addr in f])
-f.close()
-gc.get_coords(addresses, 'data/post_offices_coords.json')
+# f = open('data/post_offices.txt', 'r')
+# addresses = list([addr.strip() for addr in f])
+# f.close()
+# gc.get_coords(addresses, 'data/post_offices_coords.json')
